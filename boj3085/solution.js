cfe9dfc6140ea2c2691a59e01f642da939d4ed9f
sungmin-choi <namja306@naver.com>
2021-10-26 23:05:34 +0900

    push

---
+++ boj3085/solution.js
@@ -0,0 +1,5 @@
+const filePath = require("fs").readFileSync('C:/Users/USER/Desktop/코테협업js/JS-ThinkStack/sungmin/4주차/boj3085/input.txt', "utf8");
+//const filePath = require("fs").readFileSync('/dev/stdin', "utf8");
+let input = filePath.trim().split("\n");
+const N=input[0];
+input.shift();
