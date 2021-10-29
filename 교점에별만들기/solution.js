43255717bd51a6da14e423f683f7c4a761d46607
sungmin-choi <namja306@naver.com>
2021-10-29 18:19:55 +0900

    해결

--- 교점에별만들기/solution.js
+++ 교점에별만들기/solution.js
@@ -27,17 +27,15 @@ function solution(line){
     console.log(crossing);
     let row = crossing.map(ele=>ele[1]);
     let column = crossing.map(ele=>ele[0]);
-    let zeroPoint=[Math.max(...row),Math.max(...column)];
+    let colMin=Math.min(...column);
+    let rowMax=Math.max(...row);
     row = Math.max(...row)-Math.min(...row)+1;
-    
     column=Math.max(...column)-Math.min(...column)+1;
-    
-    console.log(zeroPoint);
     let result = Array.from({length:row},()=>Array.from({length:column},()=>'.'));
     crossing.forEach(ele=>{
         let [x,y]=ele;
-        x=x+zeroPoint[1];
-        y=zeroPoint[0]-y;
+        x=x-colMin;
+        y=rowMax-y;
         result[y][x]='*';
 
     })
