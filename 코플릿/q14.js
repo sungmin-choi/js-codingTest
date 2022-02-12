7747c75163f0af811875a06dc983cfac8f4069db
sungmin-choi <namja306@naver.com>
2022-02-12 21:29:57 +0900

    14번 제대로 못품

--- 코플릿/q14.js
+++ 코플릿/q14.js
@@ -1,20 +1,23 @@
 const rotatedArraySearch = function (rotated, target) {
-    let result = -1;
-    const binarySearch = (rotated,start,end,target)=>{
-      if(start>end){
-        return ;
+  let start = 0;
+  let end = rotated.length-1;
+  while(start<=end){
+    const middle = parseInt((start+end)/2);
+    if(rotated[middle] === target) return middle;
+    if(rotated[middle]<rotated[start]){
+      if(rotated[middle]<target && rotated[end]>=target){
+        start=middle+1
+      }else{
+        end=middle-1;
       }
-      const pivot = parseInt((start+end)/2);
-      if(rotated[pivot] === target){
-        result = pivot;
-        return;
+    } else{
+      if(rotated[start]<=target && rotated[middle]>target){
+        end=middle-1;
+      }else{
+        start=middle+1;
       }
-      binarySearch(rotated,0,pivot-1,target);
-      binarySearch(rotated,pivot+1,rotated.length,target);
-    }
-    binarySearch(rotated,0,rotated.length,target);
-    return result;
-};
 
-console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2))
-console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100))
+    }
+  }
+  return -1;
+};
