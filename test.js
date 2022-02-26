5a52f4fda25a9c90bdd738a99421dcf9c07350a9
sungmin-choi <namja306@naver.com>
2022-02-26 23:02:00 +0900

    dd

--- test.js
+++ test.js
@@ -1,33 +1,5 @@
-function test3(insertEdges, removeEdges) {
-  let size = 0;
-  insertEdges.forEach((edge)=>{
-    const [node1,node2] = edge;
-    if(size<node1) size = node1;
-    if(size<node2) size = node2;
-  })
-let matrix = Array(size+1).fill(0).map(()=>Array(size+1).fill(0));
- insertEdges.forEach((edge)=>{
-    const [node1,node2] = edge;
-    matrix[node1][node2] =1;
-    matrix[node2][node1] =1;
- })
- removeEdges.forEach((edge)=>{
-   const [node1,node2] = edge;
-    matrix[node1][node2] =0;
-    matrix[node2][node1] =0;
- })
-
- return matrix;
-
+for(let i =0;i<3;i++){
+  var a=2;
 }
 
-const insertEdges = [
-  [0, 3],
-  [0, 2],
-  [1, 3],
-  [2, 1],
-];
-const removeEdges = [[1, 3], [1, 0]];
-let output1 = test3(insertEdges, removeEdges);
-
-console.log(output1);
+console.log(a);
